f503e011c8c4848d1e96f157aa33486c3a54f106
Thomas Neil <12561498+tn819@users.noreply.github.com>
2019-04-23 10:23:55 +0200

    decent pagination

--- public/js/script.js
+++ public/js/script.js
@@ -60,7 +60,9 @@
         el: "#main",
         data: {
             page: 1,
+            imagePerPage: 10,
             images: [],
+            pages: [],
             form: {
                 title: "",
                 description: "",
@@ -111,9 +113,18 @@
                 } else {
                     return;
                 }
+            },
+            images() {
+                this.setPages();
             }
         },
-        mounted: function() {
+        computed: {
+            displayedImages() {
+                return this.paginate(this.images);
+            }
+        },
+
+        created: function() {
             var self = this;
             window.addEventListener("hashchange", function() {
                 self.modalImage.id = location.hash.slice(1);
@@ -156,6 +167,7 @@
                 console.log(this.modalImage, "receiving image");
             },
             closeModal: function() {
+                var self = this;
                 this.showModal = false;
                 this.modalImage = {};
                 this.modalImage.id = null;
@@ -164,7 +176,21 @@
                     document.title,
                     window.location.pathname + window.location.search
                 );
-                window.location.href = window.location.href;
+                if (this.images.length == 0) {
+                    axios
+                        .get("/images")
+                        .then(function(resp) {
+                            self.images = resp.data;
+                            if (!window.location.href) {
+                                window.location.href = window.location.href;
+                            } else {
+                                window.location.reload(true);
+                            }
+                        })
+                        .catch(function(err) {
+                            console.log(err);
+                        });
+                }
             },
             handleFileChange: function(e) {
                 this.form.file = e.target.files[0];
@@ -180,6 +206,22 @@
                     self.images.unshift(resp.data[0]);
                     self.$forceUpdate();
                 });
+            },
+            setPages: function() {
+                var numberOfPages = Math.ceil(
+                    this.images.length / this.imagePerPage
+                );
+                for (var index = 1; index <= numberOfPages; index++) {
+                    this.pages.push(index);
+                }
+            },
+            //I owe a lot to the approach suggested in this post: https://medium.com/@obapelumi/pagination-with-vuejs-1f505ce8d15b
+            paginate: function(posts) {
+                var page = this.page;
+                var perPage = this.imagePerPage;
+                var from = page * perPage - perPage;
+                var to = page * perPage;
+                return this.images.slice(from, to);
             }
         }
     });
